e7de73cddc5563acdf63ff9176eb97ba6936f9c4
Ildar.Karimov <karimovildar2000@gmail.com>
2023-09-11 20:36:02 +0300

    Add structure to header

--- src/components/layout/PageHeader.vue
+++ src/components/layout/PageHeader.vue
@@ -8,16 +8,29 @@
       src="src/assets/vector/logo-large.svg"
       alt="watch movies"
   >
+  <input
+      type="text"
+      class="page-header__search"
+      placeholder="Введите для поиска"
+  >
+  <div class="page-header__user-block">
+    Войти
+  </div>
 </header>
 </template>
 
 <style lang="scss" scoped>
 @use 'src/assets/scss/const/pallete/gray' as *;
+@use 'src/assets/scss/const/pallete/font-colors' as *;
+
 .page-header {
   display: flex;
   align-items: center;
+  justify-content: space-between;
   height: 72px;
   padding: 0 72px;
+
+  color: map-get($fontColors, primary);
 
   background-color: map-get($grayColors, 900);
 }
